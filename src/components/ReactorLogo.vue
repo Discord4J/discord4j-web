2f0e576730ef265d8c45613a7540b095fded1508
Ze Hao Xiao <ze.hao.xiao@oath.com>
2018-05-31 18:01:08 -0700

    update reactor logo with css styling instead of inline

--- src/components/ReactorLogo.vue
+++ src/components/ReactorLogo.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <svg xmlns="http://www.w3.org/2000/svg" width="86.43" height="80" viewBox="0 0 22.868 21.167">
+  <svg class="reactor-logo" xmlns="http://www.w3.org/2000/svg" width="86.43" height="80" viewBox="0 0 22.868 21.167">
     <g transform="matrix(.09317 0 0 .09317 -1.188 -4.467)">
-      <circle r="27.781" cy="161.533" cx="135.467" fill="#7289da"/>
-      <ellipse ry="75.093" rx="118.088" cy="161.533" cx="135.467" fill="none" stroke="#7289da" stroke-width="9.26"/>
-      <ellipse transform="rotate(60)" ry="75.093" rx="118.088" cy="-36.551" cx="207.625" fill="none" stroke="#7289da" stroke-width="9.26"/>
-      <ellipse transform="rotate(120)" ry="75.093" rx="118.088" cy="-198.084" cx="72.159" fill="none" stroke="#7289da" stroke-width="9.26"/>
+      <circle r="27.781" cy="161.533" cx="135.467"/>
+      <ellipse ry="75.093" rx="118.088" cy="161.533" cx="135.467" fill="none" stroke-width="9.26"/>
+      <ellipse transform="rotate(60)" ry="75.093" rx="118.088" cy="-36.551" cx="207.625" fill="none" stroke-width="9.26"/>
+      <ellipse transform="rotate(120)" ry="75.093" rx="118.088" cy="-198.084" cx="72.159" fill="none" stroke-width="9.26"/>
     </g>
   </svg>
 </template>
@@ -16,5 +16,15 @@ import { Component, Vue } from "vue-property-decorator"
 export default class ReactorLogo extends Vue {}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import "../colours.scss";
+
+.reactor-logo {
+  circle {
+    fill: $blurple;
+  }
+  ellipse {
+    stroke: $blurple;
+  }
+}
 </style>
---
+++ src/views/BlogPost.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  section.section.blog
+    .container
+      .blog-post
+        h1 The Site
+        p Discord4J is undergoing major revisions as part of version 3.0. Part of this includes a brand new website! Currently the site is missing some areas including: a dedicated wiki, CI, and javadocs section. But I hope to add these in the near future, in the meantime they redirect to our current hosts.
+        h1 Other Stuff
+        p In addition to the new site, we are currently planning the full rewrite of Discord4J. The dev team is working hard to make sure this is well thought-out, powerful and intuitive. We expect for Discord4J 3.0 to have some form of release within the next few months. Additionally, development of Discord4J will be done under our new Discord4J Github Organization, as such we plan to move the current repository to this organization once Discord4J 3.0 is near completion.
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component
+export default class BlogPost extends Vue {}
+</script>
+
+<style lang="scss">
+@import "../colours.scss";
+
+.blog-post {
+  h1 {
+    color: #363636;
+    font-size: 2rem;
+    font-weight: 600;
+    line-height: 1.125;
+  }
+
+  h1:not(:last-child) {
+    margin-bottom: 1.5rem;
+  }
+}
+</style>
