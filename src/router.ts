f0a4fab46e605629575726eeebed362b7c773a5c
Kevin Xiao <xlzqwerty1@gmail.com>
2018-06-01 23:11:24 -0700

    add 404 page

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import Vue from "vue"
 import Router from "vue-router"
-import Home from "./views/Home.vue"
-import Blog from "./views/Blog.vue"
+import Home from "@/views/Home.vue"
+import Blog from "@/views/Blog.vue"
+import NotFound from "@/views/NotFound.vue"
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
@@ -16,6 +18,10 @@ export default new Router({
       path: "/blog",
       name: "blog",
       component: Blog,
+    },
+    {
+      path: "*",
+      component: NotFound,
     },
   ],
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  section.section.not-found
+    h1.title Oops.
+    p.subtitle This page doesn't exist!
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component
+export default class NotFound extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
