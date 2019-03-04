9209eff98ebbfc8fc082afd7ce7ae813a0e36842
Kevin Xiao <kxiao@lyft.com>
2019-03-04 11:03:04 -0800

    update getting started links

--- src/components/home/GettingStarted.vue
+++ src/components/home/GettingStarted.vue
@@ -5,11 +5,11 @@
         .column
           h2.title Getting Started
           p Discord4J is split up into different modules for maximum flexibility. You may pick and use only what you need from the 
-            a(href="https://github.com/Discord4J/Discord4J/tree/v3#modules") modules list
+            a(href="https://github.com/Discord4J/Discord4J#modules") modules list
             | .
           br
           p To interact with all facets of the Discord API built on top of all the other modules, import the 
-            a.code(href="https://github.com/Discord4J/Discord4J/tree/v3#core")
+            a.code(href="https://github.com/Discord4J/Discord4J#core")
               code.inline core
             |  module into your favourite dependency manager (see the example configurations).
         .column#snippets
